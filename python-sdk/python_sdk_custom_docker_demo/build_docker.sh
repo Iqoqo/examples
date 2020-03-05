5cd27595a3c65d452eabace4e3a69fffc9cb72f8
Raymond Lo <raymond.lo@samsungnext.com>
2020-03-05 14:06:02 -0800

    remove run line from the build docker script

--- python-sdk/python_sdk_custom_docker_demo/build_docker.sh
+++ python-sdk/python_sdk_custom_docker_demo/build_docker.sh
@@ -2,8 +2,9 @@
 
 #build the docker image, will take a while and this requires internet connection.
 docker build -t raymondlo84/disco_python_sdk -f disco.dockerfile .
+
 #run the docker image to verify it works locally
-docker run -it raymondlo84/disco_python_sdk:latest
+#docker run -it raymondlo84/disco_python_sdk:latest
 
 #push this to the repository (change this to your own path)
 #docker push raymondlo84/disco_python_sdk:latest
