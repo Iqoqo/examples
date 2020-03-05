6bc9c223eb6aa42d5e5ad5a7571882b0a87bf240
tim-spiglanin <tims@samsungnext.com>
2020-03-04 16:52:55 -0800

    Rewrite ImageAI Readme Cleanup ImageAI requirements add gitignore for ImageAI Add automatic download of yolo weights file Move output to run-result folder

--- ImageAI/FirstObjectDetection.py
+++ ImageAI/FirstObjectDetection.py
@@ -1,14 +1,25 @@
 from imageai.Detection import ObjectDetection
 import os
+# import requests
+import urllib.request
 
-execution_path = os.getcwd()
 
-detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
-detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "mewithmug.jpg"), output_image_path=os.path.join(execution_path ,"mewithmugnew2.jpg"), minimum_percentage_probability=30)
+if __name__ == "__main__":
+    if not os.path.exists('run-result'):
+        os.mkdir('run-result')
 
-for eachObject in detections:
-    print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"])
-    print("--------------------------------")
+    yolo_url = "https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5"
+    urllib.request.urlretrieve(yolo_url, 'yolo_weights_file')
+
+    execution_path = os.getcwd()
+
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    # detector.setModelTypeAsTinyYOLOv3()
+    detector.setModelPath( os.path.join(execution_path , "yolo_weights_file"))
+    detector.loadModel()
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "mewithmug.jpg"), output_image_path=os.path.join(execution_path ,"run-result/mewithmugnew2.jpg"), minimum_percentage_probability=30)
+
+    for eachObject in detections:
+        print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"])
+        print("--------------------------------")
